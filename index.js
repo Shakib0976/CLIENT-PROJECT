86007c1031eb5f84411bc997dfe7e0b7202ea6f9
Shakib0976 <web.shakib09@gmail.com>
2025-05-19 01:38:22 +0600

    added all functionality

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9c3fo4b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@clustershakibwebcraft.lm1t8cs.mongodb.net/?retryWrites=true&w=majority&appName=ClusterShakibWebCraft`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,6 +27,7 @@ async function run() {
         await client.connect();
 
         const coffeesCollection = client.db('coffeeDB').collection('coffees');
+        const usersCollection = client.db('coffeeDB').collection('users')
 
         app.get('/coffees', async (req, res) => {
             // const cursor = coffeesCollection.find();
@@ -75,6 +77,32 @@ async function run() {
             const query = { _id: new ObjectId(id) }
             const result = await coffeesCollection.deleteOne(query);
             res.send(result);
+        })
+
+        // users based database 
+
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result)
+        })
+
+
+        // users based post
+
+        app.post('/users', async (req, res) => {
+
+            const newUser = req.body;
+            const result = await usersCollection.insertOne(newUser);
+            res.send(result)
+
+        })
+        // users based delete
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await usersCollection.deleteOne(query);
+            res.send(result)
         })
 
 
